94335f62eb1d51ab58f64922092c0328d9ff20b4
James Baker <james.d.baker@gmail.com>
2018-07-03 13:26:27 +0100

    Update dependencies and bump version to 1.0.4

--- src/main/java/org/apache/pdfbox/rendering/RendererPackageWorkaround.java
+++ src/main/java/org/apache/pdfbox/rendering/RendererPackageWorkaround.java
@@ -27,7 +27,7 @@ public class RendererPackageWorkaround {
    */
   public RendererPackageWorkaround() throws RuntimeException {
     try {
-      pageDrawer = new PageDrawer(new PageDrawerParameters(null, null));
+      pageDrawer = new PageDrawer(new PageDrawerParameters(null, null, true));
       method = PageDrawer.class.getDeclaredMethod("createGlyph2D", PDFont.class);
     } catch (NoSuchMethodException | SecurityException | IOException e) {
       throw new RuntimeException(e);
